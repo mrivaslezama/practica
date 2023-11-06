5dbc26d4deb9d69f39a3fcabf21279a5e7f80f13
Miguel Rivas <mrivaslezama@gmail.com>
2023-11-06 10:56:11 -0300

    buscando erradicar fallas

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1>Esta pagina va a ser realmente increible</h1>
+      
+      
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
